5189067e49d4b5cd2efbea5b9e07a23f2bcf8778
denver0619 <jdrlaurian@gmail.com>
2025-01-07 14:29:07 +0800

    initial setup with mock data

--- app/src/main/java/com/digiview/workwell/ui/debug/DebugActivity.java
+++ app/src/main/java/com/digiview/workwell/ui/debug/DebugActivity.java
@@ -1,6 +1,10 @@
 package com.digiview.workwell.ui.debug;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,8 +13,14 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.digiview.workwell.R;
+import com.digiview.workwell.data.models.RoutineExercise;
+import com.digiview.workwell.ui.routine.execution.RoutineActivity;
+import com.google.common.hash.Hashing;
+
+import java.util.ArrayList;
 
 public class DebugActivity extends AppCompatActivity {
+    private static final int REQUEST_ROUTINE_ACTIVITY = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +32,38 @@ public class DebugActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        RoutineExercise exercise1 = new RoutineExercise();
+        exercise1.setExerciseName("Longus Colli Stretch");
+        exercise1.setReps(2);
+        exercise1.setDuration(5000);
+        RoutineExercise exercise2 = new RoutineExercise();
+        exercise2.setExerciseName("Quadriceps Stretch Right");
+        exercise2.setReps(2);
+        exercise2.setDuration(5000);
+        RoutineExercise exercise3 = new RoutineExercise();
+        exercise3.setExerciseName("Quadriceps Stretch Left");
+        exercise3.setReps(2);
+        exercise3.setDuration(5000);
+
+
+        Button startRoutine = findViewById(R.id.startRoutineButton);
+
+        ArrayList<RoutineExercise> exercises = new ArrayList<>();
+        exercises.add(exercise1);
+        exercises.add(exercise2);
+        exercises.add(exercise3);
+
+        startRoutine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), RoutineActivity.class);
+                intent.putExtra("ROUTINE_LOG_ID", Hashing.adler32().toString());
+                intent.putExtra("EXERCISES", exercises);
+                startActivityForResult(intent, REQUEST_ROUTINE_ACTIVITY);
+            }
+        });
     }
+
 }
